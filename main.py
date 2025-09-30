5732dae8a1f667ea0e15d33f62c61fb3a733f622
abbasikov <writeumair86@gmail.com>
2025-09-30 17:30:12 -0400

    batch processing to fetch dropbox images

--- main.py
+++ main.py
@@ -37,4 +37,10 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8000,
+        timeout_keep_alive=300,  # 5 minutes
+        timeout_graceful_shutdown=30
+    )
