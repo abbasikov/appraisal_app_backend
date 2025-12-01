011c374ae8d117739b9365cbdd626a7319556113
Umair ABBASI <writeumair86@gmail.com>
2025-12-02 00:52:00 +0500

    address dynamic fix

--- app/schemas/client.py
+++ app/schemas/client.py
@@ -40,6 +40,7 @@ class ClientResponse(BaseModel):
     name: str
     parent_account_id: Optional[int] = None
     parent_account_name: Optional[str] = None  # From joined account table
+    parent_account_type: Optional[str] = None  # From joined account table
     parent_account_address: Optional[str] = None  # From parent account
     parent_account_city: Optional[str] = None  # From parent account
     parent_account_state: Optional[str] = None  # From parent account
